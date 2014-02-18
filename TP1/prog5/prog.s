eecb6234c3b8ce607fcc9080f741daad01da84a3
Rodrigo Cunha <rlopescunha17@gmail.com>
2014-02-18 11:21:09 +0000

    prog5.s tp1 debbug

--- TP1/prog5/prog.s
+++ TP1/prog5/prog.s
@@ -15,30 +15,35 @@ str3:	.space 2*STR_MAX_SIZE+1
 	.globl main
 main:
 	la $a0, str_int
-	li $v0, READ_STR
+	li $v0, PRINT_STR
+	syscall
 	#
+
 	la $a0, str1
 	li $a1, STR_MAX_SIZE
+	li $v0, 9
 	syscall
 	#
 	la $a0, str2
 	li $a1, STR_MAX_SIZE
+	li $v0,9
 	syscall
 	#
 	la $a0, str_res
-	li $v0, READ_STR
+	li $v0, PRINT_STR
+	syscall
 	#
 	la $a0, str1
 	jal strlen
+	li $a1,10
 	move $a0, $v0
-	li $a1, 10
 	li $v0, PRINT_INT
 	syscall
 	#
 	la $a0, str2
 	jal strlen
+	li $a1,10
 	move $a0, $v0
-	li $a1, 10
 	li $v0, PRINT_INT
 	syscall
 	#
@@ -123,11 +128,11 @@ strlen:
 	li $t0, 0
 strlen_for:
 	lb $t1, 0($a0)
-	beq $t1, '\0', strlen_endfor
+	beqz $t1, strlen_endfor
 	addiu $t0, $t0, 1
 	addiu $a0, $a0, 1
 	b strlen_for
-strlen_endfor:
+strlen_endfor
 	move $v0, $t0
 	jr $ra
 
