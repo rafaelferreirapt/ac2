33a3e7e1799891b66a2430cc441933d2a04c5638
Rafael Ferreira <mail@rafaelferreira.pt>
2014-02-18 17:40:08 +0000

    prog4

--- TP2/prog4/prog.s
+++ TP2/prog4/prog.s
@@ -1,23 +1,29 @@
 	.text
 	.globl main
 main:
-	li $v0,12
-	syscall
-
+	li $v0,12 
+	syscall # reset timer
+	li $t0, 0 # s contador
 while:	
 	li $v0,11
-	syscall
-
-	move $a0,$v0
-	li $v0,7
-	syscall
-
+	syscall # read core timer
+	div $t1, $v0, 20000000	
+	
+	#start if
+	beq $t1, $t0, while
+	#
+	move $a0, $t0
+	li $v0, 7
+	syscall # print contador
+	#
+	addiu $t0, $t0, 1
+	#
 	li $a0, '\n'
-	li $v0,3
-	syscall
+        li $v0,3
+        syscall
+	#endif
 	
 	b while
-
+	
 	li $v0,0
-
 	jr $ra
