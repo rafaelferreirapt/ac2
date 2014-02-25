29ec2e2fde98b877615faa8e0f9e8a6c0d25691f
rafael.ferreira@ua.pt <rafael.ferreira@hpteste2>
2014-02-25 17:20:26 +0000

    parte 1 tp3

--- TP3/parte1prog3/prog.s
+++ TP3/parte1prog3/prog.s
@@ -19,6 +19,7 @@ for:
 	andi $t3, $t2, 0x0040				#Mask para o bit6
 	andi $t4, $t2, 0x0080				#Mask para o bit7
 	srl $t4, $t4, 1
+
 	and $t5, $t3, $t4
 	or $t2, $t3, $t4
 	xor $t6, $t3, $t4
@@ -28,6 +29,15 @@ for:
 	srl $t2,$t2,5
 	srl $t6, $t6,4
 	srl $t7,$t7,3
-
+		
 	lw $t8, LATE($t1)
 	andi $t8,$t8, 0xFFF0
+	
+	or $t8, $t8, $t5
+	or $t8, $t8, $t2
+	or $t8, $t8, $t6
+	or $t8, $t8, $t7
+	
+	sw $t8, LATE($t1)
+	b for
+	jr $ra
