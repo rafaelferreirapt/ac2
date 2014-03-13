f77769db516dfa3d0bd70bdc52a5d792e525b969
Rafael Ferreira <mail@rafaelferreira.pt>
2014-03-13 13:21:22 +0000

    TP4 prog10

--- TP4/prog10/prog.c
+++ TP4/prog10/prog.c
@@ -3,10 +3,15 @@ void delay(unsigned int);
 
 int main(void){
 	char c = 0;
+	int i;
+
 	while(1){
-		delay(200);
-		send2displays(c);
+		i = 0;
+		do{
+			delay(50);
+			send2displays(c);
+		}while(++i<4);
 		c++&0x00FF;
-	}
+	};
 	return 0;
 }
