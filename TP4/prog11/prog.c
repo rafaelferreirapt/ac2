5445fb283e7559dc283eb2abd2f2d66301df085a
Rafael Ferreira <mail@rafaelferreira.pt>
2014-03-13 13:32:22 +0000

    TP4 prog11

---
+++ TP4/prog11/prog.c
@@ -0,0 +1,17 @@
+void send2displays(char);
+void delay(unsigned int);
+
+int main(void){
+	char c = 0;
+	int i;
+
+	while(1){
+		i = 0;
+		do{
+			delay(10);
+			send2displays(c);
+		}while(++i<4);
+		c++&0x00FF;
+	};
+	return 0;
+}
