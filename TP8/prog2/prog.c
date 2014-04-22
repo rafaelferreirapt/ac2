b1cfdd6e83bc4d4a5509b53875ee6588f3119e5d
Rodrigo Cunha <RodrigoCunha@aluno-5359.wireless.ua.pt>
2014-04-22 17:19:31 +0100

    prog1e2

---
+++ TP8/prog2/prog.c
@@ -0,0 +1,45 @@
+#include <detpic32.h>
+
+void delay(unsigned int);
+void putc(char);
+
+int main(void){
+	//configuration UART1
+
+	U1BRG = (20000000 / (16*115200))-1;
+	U1MODEbits.BRGH = 0;
+
+	U1MODEbits.PDSEL = 00; 
+
+	/*
+	PDSEL<1:0>: Parity and Data Selection bits 
+	11 = 9-bit data, no parity
+	10 = 8-bit data, odd parity
+	01 = 8-bit data, even parity
+	00 = 8-bit data, no parity
+	*/
+
+	U1MODEbits.STSEL = 0;
+
+	/*
+	STSEL: Stop Selection bit 
+	1 = 2 Stopbits
+	0 = 1 Stopbit
+	*/
+
+	U1STAbits.UTXEN = 1;
+	U1STAbits.URXEN = 1;
+
+	U1MODEbits.ON = 1;;
+
+	while(1){
+		putc('+');
+		delay(1000);
+	}
+}
+
+void putc(char byte2send){
+	while(U1STAbits.UTXBF);
+
+	U1TXREG = byte2send;
+}
